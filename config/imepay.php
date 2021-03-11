0423a511cb314b0aaed3b1d32c37e230593e6b10
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2021-03-11 04:24:54 +0000

    add config file

--- config/imepay.php
+++ config/imepay.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-
+    'merchant_number' => env('IME_PAY_MERCHANT_NUMBER'),
+    'merchant_name' => env('IME_PAY_MERCHANT_NAME'),
+    'merchant_code' => env('IME_PAY_MERCHANT_CODE'),
+    'merchant_module' => env('IME_PAY_MERCHANT_MODULE'),
+    'username' => env('IME_PAY_USERNAME'),
+    'password' => env('IME_PAY_PASSWORD'),
+    /**
+     * The payment url
+     * 
+     * E.g. https://stg.imepay.com.np:1234
+     */
+    'payment_url' => env('IME_PAY_PAYMENT_URL'),
 ];
